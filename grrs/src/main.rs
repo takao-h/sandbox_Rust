9b12daacbaa7063c02ea3a4ee62db45434559508
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-05-08 22:42:18 +0900

    added args structure

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,15 @@
+#[derive(Debug)]
+let pattern = std::env::args().nth(1).expect("no pattern given");
+let path = std::env::args().nth(2).expect("no path given");
+// struct Cli {
+//     pattern: String,
+//     path: std::path::PathBuf
+// }
+let args = Cli {
+    pattern: pattern,
+    path: std::path::PathBuf::from(path),
+}
+
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    
     println!("Hello, world!");
 }
