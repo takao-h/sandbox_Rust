7d785fda243545a0b818d815b06b01218b94fcff
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-05-08 23:13:14 +0900

    fixed structure and debug

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,15 +1,17 @@
-#[derive(Debug)]
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
 let pattern = std::env::args().nth(1).expect("no pattern given");
 let path = std::env::args().nth(2).expect("no path given");
-// struct Cli {
-//     pattern: String,
-//     path: std::path::PathBuf
-// }
-let args = Cli {
-    pattern: pattern,
-    path: std::path::PathBuf::from(path),
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
 
+
+
 fn main() {
+    let args = Cli::from_args();
     println!("Hello, world!");
 }
