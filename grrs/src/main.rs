0a0d4962fdac7dd964eb6cbbc059dfd6fcd6ffa1
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-05-09 23:43:45 +0900

    added params and fixed struct

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,17 +1,22 @@
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-let pattern = std::env::args().nth(1).expect("no pattern given");
-let path = std::env::args().nth(2).expect("no path given");
 struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    pattern1: String,
+    pattern2: String,
 }
 
-
-
 fn main() {
+// let result = std::fs::read_to_string("test.txt").unwrap();
+// match result {
+//     Ok(content) => { println!("File content: {}", content); }
+//     Err(error) => { println!("Oh noes: {}", error); }
+// }
+    let pattern1 = std::env::args().nth(1).expect("no pattern given");
+    let pattern2 = std::env::args().nth(2).expect("no pattern given");
+    let args = Cli {
+        pattern1: pattern1,
+        pattern2: pattern2,
+    };
     let args = Cli::from_args();
-    println!("Hello, world!");
 }
