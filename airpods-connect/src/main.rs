b935f295127ab049adf0ed9bd164631cc80a2519
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-06-12 00:42:00 +0900

    devided functions

--- airpods-connect/src/main.rs
+++ airpods-connect/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let input = &args[1];
+    // let args: Vec<String> = std::env::args().collect();
+    // let input = &args[1];
 
     // let success_message = "connected!";
     // let _error_message = "faild connect";
-    println!("{}", input);
+    println!("{}", get_mac_adress());
+}
+
+fn get_mac_adress() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    return args[1].to_string();
 }
