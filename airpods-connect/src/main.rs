227db534941eb04cfbd926a6062a37bfaf34f3fe
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-06-21 23:44:11 +0900

    refactor

--- airpods-connect/src/main.rs
+++ airpods-connect/src/main.rs
@@ -2,8 +2,6 @@ fn main() {
     // let args: Vec<String> = std::env::args().collect();
     // let input = &args[1];
 
-    // let success_message = "connected!";
-    // let _error_message = "faild connect";
     println!("{}", get_mac_adress());
 }
 
