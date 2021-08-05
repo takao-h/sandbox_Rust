24d2f1afc8148a0cbcdb0307cfe02e7aee7f7934
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-08-05 20:51:13 +0900

    added example that Json operation

---
+++ sample_axum/example/json.rs
@@ -0,0 +1,31 @@
+use axum::{
+  extract,
+  response,
+  prelude::*,
+};
+use serde::{Serialize, Deserialize};
+use std::net::SocketAddr;
+
+# [tokio::main]
+async fn main() {
+  let app = route("/ping", post(ping));
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+  hyper::Server::bind(&addr).server(app.into_make_service()).await.unwrap();
+}
+
+#[derive(Desrialize)]
+struct Ping {
+  count: i64,
+}
+
+#[derive(Serialize)]
+struct Pong {
+  count: i64,
+}
+
+async fn ping(extract::Json(ping): extract::Json<Ping>) -> response::Json<Pong> {
+  response::Json(Pong {
+    count: ping.count + 1,
+  })
+}
