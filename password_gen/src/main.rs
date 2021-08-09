35c8d513ea0ecd4f1a3072affe9bb4aeadf6d809
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-08-09 23:48:04 +0900

    deleted unuse code

--- password_gen/src/main.rs
+++ password_gen/src/main.rs
@@ -23,28 +23,8 @@ fn main() {
     }
     println!("{}", result);
     add_clipboard(result);
-
 }
 
-// fn gen_rand_str() {
-//     let mut alphabet_vec: Vec<char> = vec![];
-
-//     for _num in 1..6 {
-//         // Unicodeのコードポイントのために、97から122の乱数を発生させています
-//         let rand_num = rand::thread_rng().gen_range(97..123);
-//         if let Some(rand_num) = std::char::from_u32(rand_num) {
-//             alphabet_vec.push(rand_num);
-//         }
-//     }
-
-//     let alphabet_str: String = alphabet_vec.iter().collect::<String>();
-//     println!("{}", alphabet_str);
-// }
-
-// fn copy_password(password: String) {
-//     println!("{}", "copy password!!");
-//     println!("{}", password);
-// }
     fn add_clipboard(cory_word: String) {
         let mut clipboard = Clipboard::new().unwrap();
         clipboard.set_text(cory_word.into()).unwrap();
@@ -57,5 +37,4 @@ fn main() {
 //         let pat = "hello world";
 //         add_clipboard(pat);
 //     }
-
 // }
