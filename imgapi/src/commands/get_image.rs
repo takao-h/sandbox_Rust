7bf123a8f0e27cb055be16adf7d4c1b7e47c72f7
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-12-02 22:31:44 +0900

    added commands

---
+++ imgapi/src/commands/get_image.rs
@@ -0,0 +1,16 @@
+use crate::commands::map_to_api_key;
+use crate::opt::GetImageInput;
+use crate::result::Result;
+use imgur_openapi::apis::configuration::Configuration;
+use imgur_openapi::apis::image_api;
+
+pub async fn get_image(input: GetImageInput) -> Result<()> {
+  let mut configuration = Configuration::new();
+  configuration.api_key = map_to_api_key(input.client_id);
+  configuration.oauth_access_token = input.access_token;
+
+  let model = image_api::get_image(&configuration, &image_hash).await?;
+  let json = serde_json::to_string(&model);
+  print!({}, json);
+  Ok(())
+}
