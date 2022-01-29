b402425b40f3dd1f984c8cb3fe91d6b74e8b19f3
Takao Hayashi <takao.hayashi@zozo.com>
2022-01-29 16:34:42 +0900

    Added new PJ

---
+++ hello-dioxus/target/debug/deps/hello_dioxus.d
@@ -0,0 +1,5 @@
+/Users/rem/development/github.com/takao-h/sandbox_Rust/hello-dioxus/target/debug/deps/hello_dioxus: src/main.rs
+
+/Users/rem/development/github.com/takao-h/sandbox_Rust/hello-dioxus/target/debug/deps/hello_dioxus.d: src/main.rs
+
+src/main.rs:
---
+++ hello-dioxus/target/debug/hello-dioxus.d
@@ -0,0 +1,1 @@
+/Users/rem/development/github.com/takao-h/sandbox_Rust/hello-dioxus/target/debug/hello-dioxus: /Users/rem/development/github.com/takao-h/sandbox_Rust/hello-dioxus/src/main.rs
